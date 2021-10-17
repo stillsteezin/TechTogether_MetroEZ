736324e0a5e4d16ba6728faaa9fad400cfe27ea0
tdevwk <83445091+tdevwk@users.noreply.github.com>
2021-10-17 01:03:24 -0700

    updated firebase.js
    added web app's Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
+  apiKey: "AIzaSyCeRiDzGfvrJa8V2xXbzgnSrzY8n4OP7_I",
+  authDomain: "techtalkny-6c3c6.firebaseapp.com",
   databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
+  projectId: "techtalkny-6c3c6",
+  storageBucket: "techtalkny-6c3c6.appspot.com",
+  messagingSenderId: "288392820027",
 };
 
 const fire = firebase.intializeApp(config);
